061d9d3f33b58d5ac885a817c934060728b735c1
kokolo2020 <66770576+kokolo2020@users.noreply.github.com>
2026-08-24 07:29:05 +0700

    Refresh PWA cache for v2.9

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
-const CACHE="rithyna-meds-v1";
-const ASSETS=["./","./index.html","./manifest.webmanifest","./icon.svg"];
+const CACHE="rithyna-meds-v2.9";
+const ASSETS=["./","./index.html","./manifest.webmanifest","./icon.svg","./message-enhancements.js?v=2.9"];
 self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
-self.addEventListener("activate",e=>e.waitUntil(self.clients.claim()));
+self.addEventListener("activate",e=>e.waitUntil(Promise.all([self.clients.claim(),caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))])));
 self.addEventListener("fetch",e=>{
   if(e.request.method!=="GET") return;
   e.respondWith(fetch(e.request).then(r=>{
